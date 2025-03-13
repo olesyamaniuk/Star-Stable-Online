b004fc1a2299b1d9a486fd3aeec8c35f55d7c9f0
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-03-13 17:47:38 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -1,25 +1,5 @@
-
-
 $(function () {
-  $('.gallery__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.gallery-list-left',
-    nextArrow: '.gallery-list-right',
-  });
-});
-
-
-$(function () {
-  $('.reviews-list').slick({
+  $(".gallery__list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -29,15 +9,39 @@ $(function () {
     speed: 1000,
     dots: true,
     centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.reviews-list-left',
-    nextArrow: '.reviews-list-right',
+    cssEase: "linear",
   });
 });
 
-
-
-
-
-
-
+$(function () {
+  $(".reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".reviews-list-left",
+    nextArrow: ".reviews-list-right",
+  });
+});
+$(function () {
+  $(".features-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".features-list-left",
+    nextArrow: ".features-list-right",
+  });
+});
